dd7564ff6d0667b7957ee9b9c2cd8889e0d25027
vnavesnoj <vnavesnoj@gmail.com>
2024-05-13 18:33:20 +0300

    First commit

---
+++ olx-ads-loader-common/src/main/java/vnavesnoj/olx_ads_loader_common/OlxAdsLoaderCommonApplication.java
@@ -0,0 +1,13 @@
+package vnavesnoj.olx_ads_loader_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxAdsLoaderCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxAdsLoaderCommonApplication.class, args);
+	}
+
+}
---
+++ olx-ads-loader-common/src/test/java/vnavesnoj/olx_ads_loader_common/OlxAdsLoaderCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package vnavesnoj.olx_ads_loader_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxAdsLoaderCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
