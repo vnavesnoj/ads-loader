13d286b48ccabbad49d1f3ae3383df8ab5ac8c90
vnavesnoj <vnavesnoj@gmail.com>
2024-05-29 20:02:49 +0300

    Add service module

---
+++ ads-loader-service/src/main/java/vnavesnoj/adsloaderservice/AdsLoaderServiceApplication.java
@@ -0,0 +1,13 @@
+package vnavesnoj.adsloaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsLoaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsLoaderServiceApplication.class, args);
+    }
+
+}
---
+++ ads-loader-service/src/test/java/vnavesnoj/adsloaderservice/AdsLoaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vnavesnoj.adsloaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsLoaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
